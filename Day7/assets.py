a42fd9e3481314bbb46ddd945b2a8e0a2ccdef65
ananya <ananyapratapsingh7@gmail.com>
2024-06-14 09:08:15 +0530

    Added assets.py File for Hangman

---
+++ Day7/assets.py
@@ -0,0 +1,71 @@
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+words = [
+    "elephant", "banana", "grapes", "orange", "pearls", "peachy", "cherry", "plumeria", "kiwifruit", "melancholy",
+    "mangoes", "berries", "lively", "lemony", "apricots", "fights", "dating", "raising", "coconut", "pineapple",
+    "avocado", "straw", "blueberry", "rasping", "blackout", "waterfall", "papayas", "guavas", "passions", "dragon",
+    "pomegranates", "nectars", "tangent", "clement", "grapes", "lychee", "personal", "jackets", "durable", "olives",
+    "quince", "tomato", "carrot", "cucumber", "pumpkin", "squash", "zucchini", "pepper", "broccoli", "cauliflower",
+    "spinach", "lettuce", "kaleidoscope", "cabbage", "celery", "radish", "beetle", "turnip", "parsnip", "potatoes",
+    "sweets", "onions", "garlic", "ginger", "turmeric", "thyming", "rosemary", "basilica", "oregano", "mineral",
+    "parsley", "cilantro", "dillweed", "sagebrush", "chives", "lavender", "marjoram", "tarragon", "lemongrass", "bayleaf",
+    "cherryblossom", "dandelion", "rosary", "tulips", "daffodil", "sunflowers", "marigold", "lilypad", "orchids", "violets",
+    "puppies", "poppies", "peonies", "hibiscus", "lilac", "iris", "geranium", "gardenia", "jasmine", "azalea", "magnolia"
+]
+
+
