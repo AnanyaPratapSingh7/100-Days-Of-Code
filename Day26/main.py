ef3547d9de5c98f4cdc4fdf21df8b912015a7698
ananya <ananyapratapsingh7@gmail.com>
2024-07-07 08:32:10 +0530

    Handled exceptions in Password manager as well as NATO alphabet

--- Day26/main.py
+++ Day26/main.py
@@ -4,8 +4,17 @@ data = pandas.read_csv("nato_phonetic_alphabet.csv")
 
 nato_dict = {row.letter:row.code for (idx, row) in data.iterrows()}
 
-user_input = input("Enter a word : ")
+valid_word_entered = False
 
-result = [nato_dict[letter.upper()] for letter in user_input]
+while not valid_word_entered:
+
+    user_input = input("Enter a word : ")
+
+    try:
+        result = [nato_dict[letter.upper()] for letter in user_input]
+    except KeyError:
+        print("Sorry, only letters of the alphabet are allowed")
+    else:
+        valid_word_entered = True
 
 print(result)
--- Day29-30/main.py
+++ Day29-30/main.py
@@ -2,8 +2,9 @@ from tkinter import *
 from tkinter import messagebox
 import random
 import pyperclip
+import json
 
-BACKGROUND_COLOR = "#A7E6FF"
+BACKGROUND_COLOR = "white"
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
@@ -26,6 +27,32 @@ def generate_password():
     password_entry.insert(0, password)
 
     pyperclip.copy(password_entry.get())
+# ---------------------------- SEARCH PASSWORD ------------------------------#
+def search():
+    website = website_entry.get()
+
+    if len(website)==0:
+        messagebox.showerror(message="Website name cannot be blank.", title="Enter a website")
+
+    else:
+        try:
+            with open("password.json", "r") as file:
+                json_data = json.load(file)
+                website_info = json_data[website]
+
+        except FileNotFoundError:
+            messagebox.showerror(message="No Data File Found.", title="File Not Found")
+
+        except KeyError:
+            messagebox.showerror(message="Website not registered.", title=website)
+
+        else:
+            text = f"Email : {website_info["username"]}\nPassword : {website_info["password"]}"
+            messagebox.showinfo(title=website, message=text)
+            pyperclip.copy(website_info["password"])
+
+        finally:
+            website_entry.delete(0, END)            
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def add():
     website = website_entry.get()
@@ -38,13 +65,34 @@ def add():
     else:
         is_okay = messagebox.askokcancel(title="Add Passwprd", message=f"These are the details entered:\nEmail : {username}\nPassword : {password}\nWebsite : {website}\nClick 'OK' to confirm.")
         if is_okay:
-            with open(file="passwords.txt", mode='+a') as file:
-                file.write(f"{website} | {username} | {password}\n")
+            new_entry = {
+                website: {
+                    "username": username,
+                    "password": password,
+                }
+            }
+            json_data = {}
+            try:
+                with open("password.json", mode="r") as file:
+                    json_data = json.load(file)
 
-            website_entry.delete(0, END)
-            password_entry.delete(0, END)
+            except FileNotFoundError:
+                with open("password.json", mode="w") as file:
+                    json.dump(new_entry, file, indent=4)
 
-            messagebox.showinfo(message="Password has been added successfully.")
+            except json.JSONDecodeError:
+                json_data={}
+                
+            else:
+                json_data.update(new_entry)
+                with open("password.json", mode="w") as file:
+                    json.dump(json_data, file, indent=4)
+            finally:
+                website_entry.delete(0, END)
+                password_entry.delete(0, END)
+                messagebox.showinfo(message="Password has been added successfully.")
+            
+            
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
@@ -61,25 +109,27 @@ username_label = Label(text="Email/Username:",bg=BACKGROUND_COLOR)
 password_label = Label(text="Password:",bg=BACKGROUND_COLOR)
 
 # Creating entries
-website_entry = Entry(width=40,bg=BACKGROUND_COLOR, highlightthickness=0)
+website_entry = Entry(width=22,bg=BACKGROUND_COLOR, highlightthickness=0)
 website_entry.focus()
 username_entry = Entry(width=40,bg=BACKGROUND_COLOR, highlightthickness=0)
 username_entry.insert(END, "ananyapratapsingh7@gmail.com")
 password_entry = Entry(width=22,bg=BACKGROUND_COLOR, highlightthickness=0)
 
 #Creating the buttons
+search_button = Button(text="Search", width=15, command=search,bg=BACKGROUND_COLOR)
 generate_password_button = Button(text="Generate Password", width=15, command=generate_password,bg=BACKGROUND_COLOR)
 add_button = Button(text="Add", width=38, command=add,bg=BACKGROUND_COLOR)
 
 # Adding to Grid (Layout)
 canvas.grid(row=0, column=1)
 website_label.grid(row=1, column=0)
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry.grid(row=1, column=1)
 username_label.grid(row=2, column=0)
 username_entry.grid(row=2, column=1, columnspan=2)
 password_label.grid(row=3, column=0)
 password_entry.grid(row=3, column=1)
 generate_password_button.grid(row=3, column=2)
 add_button.grid(row=4, column=1, columnspan=2)
+search_button.grid(row=1, column=2)
 
 window.mainloop()
